8f4e3271d9fe974d642cc0a640c4b9e318e49940
Surya <suryasekhardaripa60461@gmail.com>
2023-01-27 00:05:47 +0530

    done

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -1,12 +1,36 @@
 package com.epam.rd.autotasks.sequence;
 import java.util.Scanner;
+import java.util.*;
 
 public class FindMaxInSeq {
     public static int max() {
 
         // Put your code here
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> val=new ArrayList<>();
+        while(true){
+            int a=sc.nextInt();
+            if(a!=0){
+                val.add(a);
+            }
+            else{
+                break;
+            }
+        }
+        if(val.size()==1){
+            return val.get(0);
+        }
+        else{
+            int maximum=val.get(0);
+            for(int i=1;i<val.size();i++){
+                if(val.get(i)>maximum){
+                    maximum=val.get(i);
+                }
+            }
+            return maximum;
+        }
 
-        return 0;
+        
     }
 
     public static void main(String[] args) {
